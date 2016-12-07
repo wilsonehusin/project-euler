1e788c1941fbcf4cfa623c4572696a85521716b6
Wilson E. Husin <wilsonehusin@gmail.com>
2016-12-07 12:17:48 -0500

    Robustness of compile script.

--- work/compile.sh
+++ work/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
 	echo "Usage: $0 compiler filename"
 	echo "Example: $0 gcc file.c"
 	echo "         $0 g++ file.cpp"
 	exit
 fi
+rm -f compiled
 $1 -Wall -Werror -g -o compiled $2
+rm -rf compiled.dSYM
